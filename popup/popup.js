a4d9c5f637d9631b0e85811d2b98564d1dd9bd3a
Emily Dirsh <edirsh@liaisonedu.com>
2017-07-03 03:50:35 -0400

    Create manifest and add basic UI scaffolding.

---
+++ popup/popup.js
@@ -0,0 +1,26 @@
+var handleAction = function(action) {
+  console.log("action", action);
+  switch (action) {
+    case "view":
+        viewGroups();
+      break;
+    default:
+  }
+  window.close();
+}
+
+var viewGroups = function() {
+  browser.tabs.create({
+    active: true,
+    url: "/group_view/group_view.html"
+  });
+}
+
+document.addEventListener("click", function (e) {
+  console.log("click", e);
+  if (e.target.classList.contains("action")) {
+    console.log("Action!");
+    handleAction(e.target.dataset.action);
+  }
+});
+k
