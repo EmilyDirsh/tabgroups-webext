833e40b9fc0234550bdc55a34a41d8319afa41db
Emily Dirsh <edirsh@liaisonedu.com>
2017-11-13 01:45:34 -0500

    Only open one group view tab at a time per window
    - Add background script
    - Add messaging to signal when to open group view
    - Save group view tab information to window using sessions API
    - Check for group view information and activate tab if it already
      exists
    - Remove group view information from window when group view tab
      is closed

--- popup/popup.js
+++ popup/popup.js
@@ -1,26 +1,8 @@
-var handleAction = function(action) {
-  console.log("action", action);
-  switch (action) {
-    case "view":
-        viewGroups();
-      break;
-    default:
-  }
-  window.close();
-}
-
-var viewGroups = function() {
-  browser.tabs.create({
-    active: true,
-    url: "/group_view/group_view.html"
-  });
-}
-
 document.addEventListener("click", function (e) {
-  console.log("click", e);
   if (e.target.classList.contains("action")) {
-    console.log("Action!");
-    handleAction(e.target.dataset.action);
+    browser.runtime.sendMessage({
+      type: "action",
+      action: e.target.dataset.action
+    });
   }
 });
-k
